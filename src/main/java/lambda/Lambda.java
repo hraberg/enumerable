790b99684534d0e58b05be844e3fc8c85942f5fe
Hakan Raberg <hakan.raberg@gmail.com>
2010-03-30 02:12:59 +0800

    Adding Lambda for non Collection related functions

---
+++ src/main/java/lambda/Lambda.java
@@ -0,0 +1,11 @@
+package lambda;
+
+public class Lambda {
+	public static <E1, E2, R> Fn1<E2, R> partial(final Fn2<E1, E2, R> lambda, final E1 arg1) {
+		return new Fn1<E2, R>() {
+			public R call(E2 arg2) {
+				return lambda.call(arg1, arg2);
+			}
+		};
+	}
+}
