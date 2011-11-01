bedc8c2d68d4eb158e59d3be55f6146e7166a64f
Hakan Raberg <hraberg@yahoo.se>
2011-11-01 01:18:57 +0000

    Adding Equinox and parameterising the OSGi test to run both

--- src/test/java/org/enumerable/lambda/support/osgi/LambdaOSGiTest.java
+++ src/test/java/org/enumerable/lambda/support/osgi/LambdaOSGiTest.java
@@ -4,6 +4,8 @@ import org.enumerable.lambda.Version;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -11,24 +13,26 @@ import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.ServiceLoader;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 import static org.enumerable.lambda.Lambda.λ;
 import static org.junit.Assert.assertEquals;
+import static org.osgi.framework.Constants.*;
 
-public class LambdaOSGiTest implements BundleActivator {
-    public void start(BundleContext context) throws Exception {
-        methodInsideOSGiContainerToBeWovenByWeavingHook(context);
-    }
+@RunWith(Parameterized.class)
+public class LambdaOSGiTest {
+    public static class TestActivator implements BundleActivator {
+        public void start(BundleContext context) throws Exception {
+            methodInsideOSGiContainerToBeWovenByWeavingHook(context);
+        }
 
-    private void methodInsideOSGiContainerToBeWovenByWeavingHook(BundleContext context) {
-        context.registerService(Callable.class, λ("Hello OSGi World").as(Callable.class), new Hashtable<String, Object>());
-    }
+        private void methodInsideOSGiContainerToBeWovenByWeavingHook(BundleContext context) {
+            context.registerService(Callable.class, λ("Hello OSGi World from " + context.getProperty(FRAMEWORK_VENDOR)).as(Callable.class), new Hashtable<String, Object>());
+        }
 
-    public void stop(BundleContext context) throws Exception {
+        public void stop(BundleContext context) throws Exception {
+        }
     }
 
     @Test
@@ -36,19 +40,26 @@ public class LambdaOSGiTest implements BundleActivator {
         installAndStart("target/enumerable-java-" + Version.getVersion() + ".jar");
         installAndStart("target/enumerable-java-" + Version.getVersion() + "-test.jar");
 
-        assertEquals("Hello OSGi World", service(Callable.class).call());
+        assertEquals("Hello OSGi World from " + context.getProperty(FRAMEWORK_VENDOR), service(Callable.class).call());
     }
 
-    BundleContext context;
-    Framework framework;
+    @Parameterized.Parameters
+    public static List<FrameworkFactory[]> frameworks() {
+        List<FrameworkFactory[]> result = new ArrayList<FrameworkFactory[]>();
+        for (FrameworkFactory frameworkFactory : ServiceLoader.load(FrameworkFactory.class))
+            result.add(new FrameworkFactory[] {frameworkFactory});
+        return result;
+    }
+
+    public LambdaOSGiTest(FrameworkFactory frameworkFactory) {
+        this.framework = frameworkFactory.newFramework(new HashMap<String, String>() {{
+            put(FRAMEWORK_STORAGE, new File("target", FRAMEWORK_STORAGE).getAbsolutePath());
+            put(FRAMEWORK_STORAGE_CLEAN, FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
+        }});
+    }
 
     @Before
     public void startOSGi() throws BundleException {
-        ServiceLoader<FrameworkFactory> loader = ServiceLoader.load(FrameworkFactory.class);
-        framework = loader.iterator().next().newFramework(new HashMap<String, String>() {{
-            put("org.osgi.framework.storage", new File("target/org.osgi.framework.storage").getAbsolutePath());
-            put("org.osgi.framework.storage.clean", "onFirstInit");
-        }});
         framework.start();
         context = framework.getBundleContext();
     }
@@ -65,4 +76,7 @@ public class LambdaOSGiTest implements BundleActivator {
     private void installAndStart(String bundle) throws BundleException {
         context.installBundle(new File(bundle).toURI().toString()).start();
     }
+
+    BundleContext context;
+    Framework framework;
 }
